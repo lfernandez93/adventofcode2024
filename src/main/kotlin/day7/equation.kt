b9de885358bbdd15ac1a106f0a20b42eb6586d07
Luis Fernandez <lsfernandez@urbe.edu.ve>
2024-12-08 13:00:39 -0500

    day 7 more playing around

--- src/main/kotlin/day7/equation.kt
+++ src/main/kotlin/day7/equation.kt
@@ -16,17 +16,61 @@ fun main() {
 
     println(leSum)
 
-    /*val newSum = eqs.entries.filter{
+    val newSum = eqs.entries.filter{
         canGetToTotal(it.key, it.value)
     }.sumOf {
         it.key
     }
 
-    println(newSum)*/
-    //5 + 10 * 74 + 30
-    println(
-        canGetToTotal(3720, listOf(5, 10 , 74, 30))
-    )
+    println(newSum)
+
+    var sum = 0L
+    eqs.entries.forEach{
+        if(isValidPt2(it.key, it.value)) {
+            sum+=it.key
+        }
+    }
+
+    println(sum)
+}
+
+fun isValid(target: Long, numbers: List<Long>) : Boolean {
+    if(numbers.size == 1) {
+        return numbers[0] == target
+    }
+
+    val accumSum = mutableListOf(numbers[0]+numbers[1])
+    accumSum.addAll(numbers.subList(2, numbers.size))
+    if(isValid(target, accumSum)) {
+        return true
+    }
+
+    val accumMult = mutableListOf(numbers[0]*numbers[1])
+    accumMult.addAll(numbers.subList(2, numbers.size))
+    return isValid(target, accumMult)
+
+}
+
+fun isValidPt2(target: Long, numbers: List<Long>) : Boolean {
+    if(numbers.size == 1) {
+        return numbers[0] == target
+    }
+
+    val accumSum = mutableListOf(numbers[0]+numbers[1])
+    accumSum.addAll(numbers.subList(2, numbers.size))
+    if(isValid(target, accumSum)) {
+        return true
+    }
+
+    val accumMult = mutableListOf(numbers[0]*numbers[1])
+    accumMult.addAll(numbers.subList(2, numbers.size))
+    if(isValid(target, accumMult)) {
+        return true
+    }
+
+    val accumConcat = mutableListOf("${numbers[0]}${numbers[1]}".toLong())
+    accumConcat.addAll(numbers.subList(2, numbers.size))
+    return isValid(target, accumConcat)
 
 }
 
@@ -38,8 +82,10 @@ fun canGetToTotal(target: Long, numbers: List<Long>) : Boolean {
 //6: 3 2
 //5: 3 2
 fun dec(target: Long, numbers: List<Long>, ith: Int): Boolean {
+    if(target <= 0) {
+        return false
+    }
     if(ith == 0) {
-        println("target = $target, numbers = ${numbers[ith]}")
         return target == numbers[ith]
     }
 
@@ -48,7 +94,7 @@ fun dec(target: Long, numbers: List<Long>, ith: Int): Boolean {
         return true
     }
 
-    if (dec(target - numbers[ith], numbers, ith - 1)) {
+    if (target >= numbers[ith] && dec(target - numbers[ith], numbers, ith - 1)) {
         return true
     }
 
@@ -78,9 +124,9 @@ fun readFile(): Map<Long,List<Long>> {
     val reader = BufferedReader(InputStreamReader(inputStream))
     reader.useLines { lines ->
         lines.forEach { line ->
-            var file = line.split(": ")
-            var eqResult  = file[0].toLong()
-            var ops = file[1].trim().split(" ")
+            val file = line.trim().split(": ")
+            val eqResult  = file[0].toLong()
+            val ops = file[1].trim().split(" ")
                 .map { it.toLong() }
                 .toCollection(ArrayList())
             equations[eqResult] = ops
